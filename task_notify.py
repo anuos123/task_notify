72260cf50a448fd5b53504c0b1a832af93425c25
AnuosLi <937134343@qq.com>
2023-09-25 15:25:19 +0800

    first commit!

---
+++ task_notify.py
@@ -0,0 +1,69 @@
+from winotify import Notification
+from importlib import reload
+import datetime
+import time
+import getpass
+import threading
+import gazu
+import update
+reload(update)
+
+def daily_morning_notify():
+    print("Good morning!")
+
+def daily_afternoon_notify():
+    print("Good Afternoon!")
+
+def run_thread(func,hour,minute):
+    while True:
+        now = time.localtime()
+
+
+def notify():
+
+    # pm弹窗时间周四/周五 5点弹窗,网页是https://kitsu.zuru.cloud/timesheets/
+    # at弹窗时间,网页是https://kitsu.zuru.cloud/my-tasks/timesheets：
+    # 日报 - 星期1-星期5,每天上午10点半和下午5点各自弹一次,持续10分钟(已填写,则不会触发)
+    # 周报 - 周四-周五,每天下午4点半 - 5点,持续20分钟(已填写,则不会触发)
+
+    # 获取当前时间
+    now = datetime.datetime.now()
+    weekday = now.strftime("%A")
+
+    # 获取当前用户名称
+    username = getpass.getuser()
+    if username=='lvy':
+        launch = 'https://kitsu.zuru.cloud/timesheets'
+    else:
+        launch = 'https://kitsu.zuru.cloud/my-tasks/timesheets'
+
+    task = f'{weekday} Task'
+    daily = r'每日任务'
+    message = f"Hi,{username}\n现在是休息时间...\n填写{daily},才能劳逸结合..."
+    icon = r"C:/Users/kangwen/Desktop/ZT_Install/logo.ico"
+
+    toast = Notification(app_id=task,
+                         title=daily,
+                         msg=message,
+                         icon=icon,
+                         duration='long',
+                         launch=launch
+                         )
+
+    toast.show()
+
+def timer():
+    # 获取当前时间
+    while True:
+        now = datetime.datetime.now()
+        hour = now.hour
+        minute = now.minute
+
+        if hour == 15:  # 判断当前时间是否超过下午3点
+            if minute >= 0 and minute<=10 :
+                time.sleep(30)
+                notify()
+        else:
+            print(f"Current Time: {now},Connecting...")
+            time.sleep(60)
+timer()
